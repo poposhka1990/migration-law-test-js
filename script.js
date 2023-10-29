8e7970f7f5f3cd7257017ca6aea0e4dd5d96cf3d
Илья Попов <ilapopov@MacBook-Air-Ila.local>
2023-10-29 18:28:02 +0300

    refactor repeating patterns

--- script.js
+++ script.js
@@ -1,83 +1,60 @@
-// не получается выынести в модуль ошибка: Cannot use import statement outside a module
-const QUESTIONS = [
-    {
-        question: 'Сколько дней предоставляется иностранному гражданину для постановки на миграционный учёт (оформления регистрации)?',
-        options: [ '1 день', '3 дня', '5 дней', '7 дней' ],
-        answer: 'fourth',
-        correctCommentary: 'Верно! На оформление регистрации предоставляется 7 рабочих дней с момента заселения в общежитие/квартиру. Именно поэтому этот документ полностью называется "отрывной талон уведомления о прибытии иностранного гражданина в место пребывания".',
-        wrongCommentary: 'Не отгадали. Правильный ответ - 7 дней. Но мы ценим, что вы даёте нам больше времени.'
-    },
-    {
-        question: 'Я не выезжала(а) из России, но получил новый документ. Сколько у меня дней для переоформления регистрации?',
-        options: [ '1 день', '3 дня', '5 дней', '7 дней' ],
-        answer: 'second',
-        correctCommentary: 'Да, так и есть! 3 дня при переоформлении и 7 дней при въезде в Россию из-за границы.',
-        wrongCommentary: 'А вот и неправильно! У вас есть всего 3 дня на это.'
-    },
-    {
-        question: 'Сколько дней оформляется приглашение для визы?',
-        options: [ '1 день', '2 недели', '1 месяц', '2 месяца' ],
-        answer: 'third',
-        correctCommentary: 'Правильно! Один месяц, учитывая составление документов, согласование и оформление.',
-        wrongCommentary: 'Неверно, оно оформляется 1 месяц'
-    }
-]
+import QUESTIONS from "./questions.js"
 
+// добавить El
 const question = document.querySelector('.question')
-const input = document.querySelectorAll('input')
-const firstOptionLabel = document.querySelector('#firstLabel')
-const secondOptionLabel = document.querySelector('#secondLabel')
-const thirdOptionLabel = document.querySelector('#thirdLabel')
-const fourthOptionLabel = document.querySelector('#fourthLabel')
+const inputs = document.querySelectorAll('input')
 const button = document.querySelector('button')
 const commentary = document.querySelector('.commentary')
-const label = document.querySelectorAll('label')
+const labels = document.querySelectorAll('label')
 const result = document.querySelector('.content')
+const questionNumberLabel = document.querySelector('.question-number')
+const progressBar = document.querySelector('#progress-bar')
+const optionLabels = [
+    document.querySelector('#firstLabel'),
+    document.querySelector('#secondLabel'),
+    document.querySelector('#thirdLabel'),
+    document.querySelector('#fourthLabel')
+]
 
-let questionNumberLabel = document.querySelector('.question-number')
 let chosenOption = ''
 let questionNumber = 0
-let chosenInput = ''
-let progressBar = document.querySelector('#progress-bar')
-let progress = 0
+let chosenIndex = ''
+let correctAnswersCount = 0
 
 // Почему если переписываю на стандартную функцию, то не работает в блоке setTimeout?
-const getNextQuestion = () => {
+function getNextQuestion () {
     if (questionNumber <= QUESTIONS.length - 1) {
         questionNumberLabel.textContent = `${questionNumber + 1} из ${QUESTIONS.length} вопросов`
         question.textContent = QUESTIONS[questionNumber].question
-        // Если блоки похожие, то нужно ли обходить циклом?
-        firstOptionLabel.textContent = QUESTIONS[questionNumber].options[0]
-        secondOptionLabel.textContent = QUESTIONS[questionNumber].options[1]
-        thirdOptionLabel.textContent = QUESTIONS[questionNumber].options[2]
-        fourthOptionLabel.textContent = QUESTIONS[questionNumber].options[3]
         commentary.textContent = ''
+
+        QUESTIONS[questionNumber].options.forEach((option, index) => {
+            optionLabels[index].textContent = option
+        })
     } else {
-        result.innerHTML = `<p>
-                             У вас ${progress} правильных ответа из ${QUESTIONS.length} вопросов
-                          </p>`
+        result.innerHTML = `<p>У вас ${correctAnswersCount} правильных ответа из ${QUESTIONS.length} вопросов</p>`
     }
 }
 
-const showProgress = () => {
+function updateProgress () {
     progressBar.value = questionNumber
 } 
 
-const uncheckInput = () => {
-    input[chosenInput].checked = false
+function uncheckInput () {
+    inputs[chosenIndex].checked = false
 }
 
-const removeStyles = () => {
-    label[chosenInput].classList.remove('wrong-answer')
-    label[chosenInput].classList.remove('right-answer')
+function removeLabelsStyles () {
+    labels[chosenIndex].classList.remove('wrong-answer')
+    labels[chosenIndex].classList.remove('right-answer')
 }
 
-getNextQuestion(questionNumber)
+getNextQuestion()
 
-input.forEach((element, i, arr) => {
+inputs.forEach((element, i) => {
     element.addEventListener('click', () => {
-      chosenOption = arr[i].value
-      chosenInput = i
+        chosenOption = element.value
+        chosenIndex = i
     })
 })
 
@@ -85,20 +62,18 @@ input.forEach((element, i, arr) => {
 button.addEventListener('click', () => {
     if (chosenOption === QUESTIONS[questionNumber].answer) {
         commentary.textContent = QUESTIONS[questionNumber].correctCommentary
-        label[chosenInput].classList.add('right-answer')
-        progress++
-        questionNumber++
-        setTimeout(removeStyles, 1000)
-        setTimeout(showProgress, 1000)
-        setTimeout(uncheckInput, 1500)
-        setTimeout(getNextQuestion, 2500)      
+        labels[chosenIndex].classList.add('right-answer')
+        correctAnswersCount++     
     } else {
         commentary.textContent = QUESTIONS[questionNumber].wrongCommentary
-        label[chosenInput].classList.add('wrong-answer')
-        questionNumber++
-        setTimeout(removeStyles, 1000)
-        setTimeout(showProgress, 1000)
-        setTimeout(uncheckInput, 1500)
-        setTimeout(getNextQuestion, 2500)    
+        labels[chosenIndex].classList.add('wrong-answer')
     }
+
+    // Тоже получается из этого сделать функцию, если повторяется?
+    questionNumber++
+    // Добавить кнопку следующий вопрос 
+    setTimeout(removeLabelsStyles, 1000)
+    setTimeout(updateProgress, 1000)
+    setTimeout(uncheckInput, 1500)
+    setTimeout(getNextQuestion, 2500) 
 })
